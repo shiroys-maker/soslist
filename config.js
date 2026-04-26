c91a9fefecf04c71b55d6778326db43ec87fe67c
shungohiroyasu <shiroys@gmail.com>
2026-04-26 23:40:42 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // APIキー設定ファイル（このファイルはGitに含まれません）
-const CLAUDE_API_KEY = "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";
+const CLAUDE_API_KEY = "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";
